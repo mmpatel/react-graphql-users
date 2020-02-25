38f03bb00569030eb767d0de039eaa3095a2e568
mukeshk <mukeshkdev15@gmail.com>
2020-02-25 19:35:51 +0530

    Refactored App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import UserContainer from './container/User/userContainer'
 
-function App() {
-  return (
-    <UserContainer />
-  )
-}
+const App = () => <UserContainer />
 
 export default App
